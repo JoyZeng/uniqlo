bdfce4fc059e7e0ec30a9fdf01cc96484744a37d
Joy <joyzeng555@gmail.com>
2019-04-23 00:14:29 -0400

    Init Spring Boot

---
+++ backend/src/main/java/me/zengyi/uniqlo/UniqloApplication.java
@@ -0,0 +1,13 @@
+package me.zengyi.uniqlo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniqloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniqloApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/me/zengyi/uniqlo/UniqloApplicationTests.java
@@ -0,0 +1,16 @@
+package me.zengyi.uniqlo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UniqloApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
