beaf1d716d045a716093e7109237f2bce9c2faef
Joy <joyzeng555@gmail.com>
2019-04-23 19:45:39 -0400

    Set up response structure.

---
+++ backend/src/main/java/me/zengyi/uniqlo/common/APIResponse.java
@@ -0,0 +1,26 @@
+package me.zengyi.uniqlo.common;
+
+import org.apache.tomcat.util.bcel.Const;
+
+import java.util.HashMap;
+
+public class APIResponse extends HashMap<String, Object> {
+
+    private static final long serialVersionUID = 7413154911246858714L;
+
+    public APIResponse success(Object result) {
+        this.put("code", Constant.API_RESPONSE_CODE_SUCCESS);
+        this.put("status", Constant.API_RESPONSE_STATUS_OK);
+        this.put("message", "");
+        this.put("result", result);
+        return this;
+    }
+
+    public APIResponse failure(Integer code, String message, Object result) {
+        this.put("code", Constant.API_RESPONSE_CODE_SERVER_ERROR);
+        this.put("status", Constant.API_RESPONSE_STATUS_ERROR);
+        this.put("message", message);
+        this.put("result", result);
+        return this;
+    }
+}
---
+++ backend/src/main/java/me/zengyi/uniqlo/common/Constant.java
@@ -0,0 +1,12 @@
+package me.zengyi.uniqlo.common;
+
+public class Constant {
+
+    public static final Integer API_RESPONSE_CODE_SUCCESS = 200;
+    public static final Integer API_RESPONSE_CODE_SERVER_ERROR = 500;
+    public static final Integer API_RESPONSE_CODE_BAD_REQUEST = 400;
+
+    public static final String API_RESPONSE_STATUS_OK = "ok";
+    public static final String API_RESPONSE_STATUS_ERROR = "error";
+
+}
